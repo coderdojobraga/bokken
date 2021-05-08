a231361091134dd6c777917f1242f5458601777a
Cecilia Soares <ceciliasoares@MBP-de-Cecilia.home>
2021-05-08 17:38:27 +0100

    Lectures initial commit

---
+++ lib/bokken_web/controllers/lecture_controller.ex
@@ -0,0 +1,43 @@
+defmodule BokkenWeb.LectureController do
+  use BokkenWeb, :controller
+
+  alias Bokken.Events
+  alias Bokken.Events.Lecture
+
+  action_fallback BokkenWeb.FallbackController
+
+  def index(conn, _params) do
+    lectures = Events.list_lectures()
+    render(conn, "index.json", lectures: lectures)
+  end
+
+  def create(conn, %{"lecture" => lecture_params}) do
+    with {:ok, %Lecture{} = lecture} <- Events.create_lecture(lecture_params) do
+      conn
+      |> put_status(:created)
+      |> put_resp_header("location", Routes.lecture_path(conn, :show, lecture))
+      |> render("show.json", lecture: lecture)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    lecture = Events.get_lecture!(id)
+    render(conn, "show.json", lecture: lecture)
+  end
+
+  def update(conn, %{"id" => id, "lecture" => lecture_params}) do
+    lecture = Events.get_lecture!(id)
+
+    with {:ok, %Lecture{} = lecture} <- Events.update_lecture(lecture, lecture_params) do
+      render(conn, "show.json", lecture: lecture)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    lecture = Events.get_lecture!(id)
+
+    with {:ok, %Lecture{}} <- Events.delete_lecture(lecture) do
+      send_resp(conn, :no_content, "")
+    end
+  end
+end
---
+++ priv/repo/migrations/20210508155951_create_lectures_mentors_assistants.exs
@@ -0,0 +1,16 @@
+defmodule Bokken.Repo.Migrations.CreateLecturesMentorsAssistants do
+  use Ecto.Migration
+
+  def change do
+    create table(:lectures_mentors_assistants, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :lecture_id, references(:lectures, on_delete: :nothing, type: :binary_id)
+      add :mentor_id, references(:mentors, on_delete: :nothing, type: :binary_id)
+
+      timestamps()
+    end
+
+    create index(:lectures_mentors_assistants, [:lecture_id])
+    create index(:lectures_mentors_assistants, [:mentor_id])
+  end
+end
