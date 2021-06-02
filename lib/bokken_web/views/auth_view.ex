d4874366a2f6b0bd25941cbb260663060ae7a980
Nelson Estevão <nelsonmestevao@gmail.com>
2021-06-02 14:29:23 +0100

    Change auth views (#51)

--- lib/bokken_web/views/auth_view.ex
+++ lib/bokken_web/views/auth_view.ex
@@ -6,46 +6,51 @@ defmodule BokkenWeb.AuthView do
   alias BokkenWeb.NinjaView
   alias BokkenWeb.OrganizerView
 
-  def render("me.json", %{user: %{active: false} = user}) do
-    render_one(user, AuthView, "user.json")
+  def render("me.json", %{user: user, registered: false}) do
+    render_one(user, AuthView, "unregistered.json")
   end
 
   def render("me.json", %{user: %{role: :guardian, guardian: guardian} = user}) do
     render_one(guardian, GuardianView, "guardian.json")
-    |> Map.merge(render_one(user, AuthView, "user.json"))
+    |> Map.merge(render_one(user, AuthView, "registered.json"))
     |> Map.put(:guardian_id, guardian.id)
   end
 
   def render("me.json", %{user: %{role: :mentor, mentor: mentor} = user}) do
     render_one(mentor, MentorView, "mentor.json")
-    |> Map.merge(render_one(user, AuthView, "user.json"))
+    |> Map.merge(render_one(user, AuthView, "registered.json"))
     |> Map.put(:mentor_id, mentor.id)
   end
 
   def render("me.json", %{user: %{role: :ninja, ninja: ninja} = user}) do
     render_one(ninja, NinjaView, "ninja.json")
-    |> Map.merge(render_one(user, AuthView, "user.json"))
+    |> Map.merge(render_one(user, AuthView, "registered.json"))
     |> Map.put(:ninja_id, ninja.id)
   end
 
   def render("me.json", %{user: %{role: :organizer, organizer: organizer} = user}) do
     render_one(organizer, OrganizerView, "organizer.json")
-    |> Map.merge(render_one(user, AuthView, "user.json"))
+    |> Map.merge(render_one(user, AuthView, "registered.json"))
     |> Map.put(:organizer_id, organizer.id)
   end
 
-  def render("user.json", %{auth: user}) do
-    registered =
-      [user.mentor, user.guardian, user.ninja, user.organizer]
-      |> Enum.any?(&(Ecto.assoc_loaded?(&1) and not is_nil(&1)))
+  def render("registered.json", %{auth: user}) do
+    base(user)
+    |> Map.merge(%{registered: true})
+  end
 
+  def render("unregistered.json", %{auth: user}) do
+    base(user)
+    |> Map.merge(%{registered: false})
+  end
+
+  defp base(user) do
     %{
       id: user.id,
       email: user.email,
       role: user.role,
       active: user.active,
-      verified: user.verified,
-      registered: registered
+      verified: user.verified
     }
   end
 end
