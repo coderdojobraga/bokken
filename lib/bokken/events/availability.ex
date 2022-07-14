83554c0782897e6dd9daf21ec45016005cec2aae
RuiL1904 <rui.lilokas2003@hotmail.com>
2022-07-14 20:35:13 +0100

    Create schema

---
+++ lib/bokken/events/availability.ex
@@ -0,0 +1,31 @@
+defmodule Bokken.Events.Availability do
+  @moduledoc """
+  The availability of the mentors for an event of CoderDojo Braga
+  """
+  use Bokken.Schema
+
+  alias Bokken.Accounts.Mentor
+  alias Bokken.Events.Event
+
+  @required_fields [:is_available?, :mentor_id, :event_id]
+  @optional_fields [:notes]
+
+  schema "availability" do
+    field :is_available?, :boolean, default: false
+    field :notes, :string
+
+    belongs_to :event, Event
+    belongs_to :mentor, Mentor
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(availability, attrs) do
+    availability
+    |> cast(attrs, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
+    |> assoc_constraint(:event)
+    |> assoc_constraint(:mentor)
+  end
+end
