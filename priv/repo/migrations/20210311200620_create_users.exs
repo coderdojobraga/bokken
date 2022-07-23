eeb94b56efde64090dc31009421e9f380c190554
Nelson Estevão <nelsonmestevao@proton.me>
2022-07-24 00:53:23 +0100

    Add citex extension to postgres

--- priv/repo/migrations/20210311200620_create_users.exs
+++ priv/repo/migrations/20210311200620_create_users.exs
@@ -2,10 +2,12 @@ defmodule Bokken.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
+    execute "CREATE EXTENSION IF NOT EXISTS citext", ""
+
     create table(:users, primary_key: false) do
       add :id, :binary_id, primary_key: true
 
-      add :email, :string, null: false
+      add :email, :citext, null: false
       add :password_hash, :string, null: false
       add :role, :string, null: false
 
