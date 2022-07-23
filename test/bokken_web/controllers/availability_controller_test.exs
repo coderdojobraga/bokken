b5ed50e174ee18c9f0cb130604211c356a99380f
RuiL1904 <rui.lilokas2003@hotmail.com>
2022-07-23 17:29:49 +0100

    Create first tests

---
+++ test/bokken_web/controllers/availability_controller_test.exs
@@ -0,0 +1,74 @@
+defmodule BokkenWeb.AvailabilityControllerTest do
+  use BokkenWeb.ConnCase
+
+  alias Bokken.Accounts
+  alias Bokken.Events
+  alias BokkenWeb.Authorization
+
+  setup %{conn: conn} do
+    {:ok, location} =
+      %{
+        address: "Test address",
+        name: "Departamento de Informática"
+      }
+      |> Events.create_location()
+
+    {:ok, team} =
+      %{
+        name: "Turma Yin",
+        description: "Uma turma"
+      }
+      |> Events.create_team()
+
+    event_fixture =
+      %{
+        title: "Test event",
+        spots_available: 30,
+        start_time: ~U[2023-02-14 10:00:00.000Z],
+        end_time: ~U[2023-02-14 12:30:00.000Z],
+        enrollments_open: ~U[2022-07-03 12:30:00.0Z],
+        enrollments_close: ~U[2023-02-13 12:30:00.0Z],
+        online: false,
+        notes: "Valentines"
+      }
+      |> Map.put(:location_id, location.id)
+      |> Map.put(:team_id, team.id)
+
+    {:ok, event} = Events.create_event(event_fixture)
+
+    {:ok, conn: put_resp_header(conn, "accept", "application/json"), event: event}
+  end
+
+  describe "index" do
+    setup [:register_and_log_in_mentor]
+
+    @tag mine: true
+    test "list all availabilities", %{conn: conn, event: event} do
+      conn = get(conn, Routes.event_availability_path(conn, :index, event.id))
+      assert json_response(conn, 200)["data"] == []
+    end
+  end
+
+  describe "create availability" do
+    setup [:register_and_log_in_mentor]
+
+    @tag mine: true
+    test "render availability when data is valid", %{conn: conn, event: event} do
+      availability_attrs = %{
+        availability: %{event_id: event.id, mentor_id: mentor.id, is_available?: true}
+      }
+
+      conn = post(conn, Routes.event_availability_path(conn, :create, event.id), availability_attrs)
+      assert %{"id" => availability_id} = json_response(conn, 201)["data"]
+
+      # conn = get(conn, Routes.event_availability_path(conn, :show, event.id, availability_id))
+      # assert json_response(conn, 200)["data"]
+
+      # conn = get(conn, Routes.event_availability_path(conn, :index, event.id))
+      # assert json_response(conn, 200)["data"]
+
+      # conn = get(conn, Routes.event_availability_path(conn, :index, mentor.id))
+      # assert json_response(conn, 200)["data"]
+    end
+  end
+end
