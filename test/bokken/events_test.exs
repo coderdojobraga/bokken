83a6425a791a9c04ac290ec238715e722810b471
Rui Oliveira <rpoliveira02@gmail.com>
2023-01-25 17:46:03 +0000

    Update events test

--- test/bokken/events_test.exs
+++ test/bokken/events_test.exs
@@ -6,141 +6,30 @@ defmodule Bokken.EventsTest do
   alias Bokken.Repo
 
   describe "enrollments" do
-    def valid_enrollment do
-      %{
-        accepted: false
-      }
-    end
-
-    def valid_event do
-      %{
-        title: "Test event",
-        spots_available: 30,
-        start_time: ~U[2023-02-14 10:00:00.000Z],
-        end_time: ~U[2023-02-14 12:30:00.000Z],
-        enrollments_open: ~U[2022-07-03 12:30:00.0Z],
-        enrollments_close: ~U[2023-02-13 12:30:00.0Z],
-        online: false,
-        notes: "Valentines"
-      }
-    end
-
-    def valid_ninja do
-      %{
-        first_name: "Joana",
-        last_name: "Costa",
-        birthday: ~U[2007-03-14 00:00:00.000Z]
-      }
-    end
-
-    def user_ninja do
-      %{
-        email: "joanacosta@gmail.com",
-        password: "ninja123",
-        role: "ninja"
-      }
-    end
-
-    def user_guardian do
-      %{
-        email: "anacosta@gmail.com",
-        password: "guardian123",
-        role: "guardian"
-      }
-    end
-
-    def valid_guardian do
-      %{
-        first_name: "Ana",
-        last_name: "Costa",
-        mobile: "+351912345678"
-      }
-    end
-
-    def valid_location do
-      %{
-        address: "Test address",
-        name: "Departamento de Informática"
-      }
-    end
-
-    def valid_team do
-      %{
-        name: "Turma Yin",
-        description: "Uma turma"
-      }
-    end
-
-    def attrs do
-      {:ok, new_ninja_user} = Accounts.create_user(user_ninja())
-      {:ok, new_guardian_user} = Accounts.create_user(user_guardian())
-
-      guardian =
-        valid_guardian()
-        |> Map.put(:user_id, new_guardian_user.id)
-
-      {:ok, new_guardian} = Accounts.create_guardian(guardian)
-
-      ninja =
-        valid_ninja()
-        |> Map.put(:guardian_id, new_guardian.id)
-        |> Map.put(:user_id, new_ninja_user.id)
-
-      {:ok, new_ninja} = Accounts.create_ninja(ninja)
-
-      {:ok, new_location} = Events.create_location(valid_location())
-
-      {:ok, new_team} = Events.create_team(valid_team())
-
-      event =
-        valid_event()
-        |> Map.put(:location_id, new_location.id)
-        |> Map.put(:team_id, new_team.id)
-
-      {:ok, new_event} = Events.create_event(event)
-
-      Map.put(valid_enrollment(), :ninja_id, new_ninja.id)
-      |> Map.put(:event_id, new_event.id)
-    end
-
-    def enrollment_fixture(attributes \\ %{}) do
-      valid_attrs = attrs()
-
-      {:ok, enrollment} =
-        Events.create_enrollment(
-          Events.get_event!(valid_attrs.event_id),
-          Enum.into(attributes, valid_attrs)
-        )
-
-      enrollment
-    end
-
-    def enrollment do
-      enrollment = enrollment_fixture()
-
-      enrollment
-      |> Map.put(:event, Events.get_event!(enrollment.event_id))
-      |> Map.put(:ninja, Accounts.get_ninja!(enrollment.ninja_id))
-    end
-
     test "list_enrollments/0 returns all enrollments" do
-      enrollment = enrollment()
-      assert Events.list_enrollments() == [enrollment]
+      enrollment = insert(:enrollment)
+      assert Events.list_enrollments()
+            |> Enum.map(fn x -> x.id end) == [enrollment.id]
     end
 
     test "list_enrollments/1 returns all enrollments of the event" do
-      enrollment = enrollment()
-      assert Events.list_enrollments(%{"event_id" => enrollment.event_id}) == [enrollment]
+      enrollment = insert(:enrollment)
+      assert Events.list_enrollments(%{"event_id" => enrollment.event_id})
+            |> Enum.map(fn x -> x.id end) == [enrollment.id]
     end
 
     test "list_enrollments/1 returns all enrollments of the ninja" do
-      enrollment = enrollment()
-      assert Events.list_enrollments(%{"ninja_id" => enrollment.ninja_id}) == [enrollment]
+      enrollment = insert(:enrollment)
+      assert Events.list_enrollments(%{"ninja_id" => enrollment.ninja_id})
+            |> Enum.map(fn x -> x.id end) == [enrollment.id]
     end
 
     test "get_enrollment/1 returns the requested enrollment" do
-      enrollment = enrollment()
-      assert Events.get_enrollment(enrollment.id, [:ninja, :event]) == enrollment
+      e1 = insert(:enrollment)
+      e2 = Events.get_enrollment(e1.id, [:ninja, :event])
+
+      assert e1.ninja_id == e2.ninja_id
+      assert e1.event_id == e2.event_id
     end
 
     test "get_enrollment/1 fails if the enrollment does not exist" do
@@ -148,29 +37,24 @@ defmodule Bokken.EventsTest do
     end
 
     test "create_enrollment/1 returns error if the enrollments are closed" do
-      valid_attrs = attrs()
-      event = Events.get_event!(valid_attrs.event_id)
+      event = insert(:event, start_time: ~U[2022-07-03 10:00:00.0Z],
+      end_time: ~U[2022-07-03 12:30:00.0Z],
+      enrollments_open: ~U[2022-07-03 07:00:00.0Z],
+      enrollments_close: ~U[2022-07-03 08:00:00.0Z])
 
-      {:ok, event} =
-        Events.update_event(event, %{
-          start_time: ~U[2022-07-03 10:00:00.0Z],
-          end_time: ~U[2022-07-03 12:30:00.0Z],
-          enrollments_open: ~U[2022-07-03 07:00:00.0Z],
-          enrollments_close: ~U[2022-07-03 08:00:00.0Z]
-        })
 
-      assert elem(Events.create_enrollment(event, valid_attrs), 0) == :error
+      assert elem(Events.create_enrollment(event,params_for(:enrollment)), 0) == :error
     end
 
     test "update_enrollment/2 updates existing enrollment" do
-      enrollment = enrollment()
+      enrollment = insert(:enrollment, accepted: false)
 
       assert Events.update_enrollment(enrollment, %{accepted: true}) ==
                {:ok, Map.put(enrollment, :accepted, true)}
     end
 
     test "update_enrollment/2 fails if the enrollment does not exist" do
-      enrollment = enrollment()
+      enrollment = insert(:enrollment)
 
       assert_raise Ecto.StaleEntryError, ~r/.*/, fn ->
         Events.update_enrollment(Map.put(enrollment, :id, Ecto.UUID.generate()), %{accepted: true})
@@ -178,17 +62,17 @@ defmodule Bokken.EventsTest do
     end
 
     test "update_enrollment/2 fails if the new value is not valid" do
-      enrollment = enrollment()
+      enrollment = insert(:enrollment)
       assert elem(Events.update_enrollment(enrollment, %{accepted: nil}), 0) == :error
     end
 
     test "delete_enrollment/1 deletes existing enrollment" do
-      enrollment = enrollment()
+      enrollment = insert(:enrollment)
       assert elem(Events.delete_enrollment(enrollment), 0) == :ok
     end
 
     test "delete_enrollment/1 returns error if the event has already occurred" do
-      enrollment_attrs = enrollment()
+      enrollment_attrs = insert(:enrollment)
       enrollment = Events.get_enrollment(enrollment_attrs.id, [:event])
 
       Events.update_event(enrollment.event, %{
@@ -202,7 +86,7 @@ defmodule Bokken.EventsTest do
     end
 
     test "delete_enrollment/1 fails if the enrollment does not exist" do
-      enrollment = enrollment()
+      enrollment = insert(:enrollment)
 
       assert_raise Ecto.StaleEntryError, ~r/.*/, fn ->
         Events.delete_enrollment(Map.put(enrollment, :id, Ecto.UUID.generate()))
