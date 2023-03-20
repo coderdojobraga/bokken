d04dc53f4b3d5328aae7b6f035aaea7bf83382ee
Nelson Estevão <nelsonmestevao@gmail.com>
2023-03-20 01:31:00 +0000

    Refactor PtMobile EctoType module (#172)

--- lib/bokken/ecto/pt_mobile.ex
+++ lib/bokken/ecto/pt_mobile.ex
@@ -1,44 +1,40 @@
 defmodule Bokken.Ecto.PtMobile do
-  @moduledoc false
-
-  @doc ~S"""
+  @moduledoc """
   PT Phone number type with validation and formatting for Ecto.
-
-  ## Examples
-
-    iex> Bokken.Ecto.PtMobile.cast("+351 912 345 678")
-    {:ok, "+351912345678"}
-    
-    iex> Bokken.Ecto.PtMobile.cast("929066855")
-    {:ok, "+351929066855"}
-
-    iex> Bokken.Ecto.PtMobile.cast("+351 939-066-855")
-    {:ok, "+351939066855"}
-
-    iex> Bokken.Ecto.PtMobile.cast("+351 979 066 855")
-    {:error, [message: "invalid PT phone number"]}
-
-    iex> Bokken.Ecto.PtMobile.cast("+351 989 066 855")
-    {:error, [message: "invalid PT phone number"]}
   """
-  import BokkenWeb.Gettext
-
   use Ecto.Type
 
+  import BokkenWeb.Gettext
+
+  @spec type :: :string
   def type, do: :string
 
   @doc """
-  Transforms external data into a runtime format
+  Transforms external data into a runtime format.
 
   ## Parameters
     - number: valid PT phone number with the "+351" indicative in a string format
+
+  ## Examples
+
+    iex> PtMobile.cast("+351 912 345 678")
+    {:ok, "+351912345678"}
+
+    iex> PtMobile.cast("929066855")
+    {:ok, "+351929066855"}
+
+    iex> PtMobile.cast("+351 939-066-855")
+    {:ok, "+351939066855"}
+
+    iex> PtMobile.cast("+351 979 066 855")
+    {:error, [message: "número de telemóvel PT inválido"]}
+
+    iex> Gettext.put_locale("en")
+    iex> PtMobile.cast("+351 989 066 855")
+    {:error, [message: "invalid PT phone number"]}
+
   """
-  def cast(number) do
-    case format(number) do
-      {:ok, number} -> {:ok, number}
-      {:error, message} -> {:error, message}
-    end
-  end
+  def cast(number), do: format(number)
 
   @doc """
   Transforms the data into a specific format to be stored
@@ -46,12 +42,7 @@ defmodule Bokken.Ecto.PtMobile do
   ## Parameters
     - number: valid PT phone number with the "+351" indicative in a string format
   """
-  def dump(number) do
-    case format(number) do
-      {:ok, number} -> {:ok, number}
-      {:error, _message} -> :error
-    end
-  end
+  def dump(number), do: format(number) |> parse_result()
 
   @doc """
   Transforms the data back to a runtime format
@@ -59,12 +50,7 @@ defmodule Bokken.Ecto.PtMobile do
   ## Parameters
     - number: valid PT phone number with the "+351" indicative in a string format
   """
-  def load(number) do
-    case format(number) do
-      {:ok, number} -> {:ok, number}
-      {:error, _message} -> :error
-    end
-  end
+  def load(number), do: format(number) |> parse_result()
 
   defp format(number) do
     {:ok, phone_number} = ExPhoneNumber.parse(number, "PT")
@@ -75,4 +61,7 @@ defmodule Bokken.Ecto.PtMobile do
       {:error, [message: gettext("invalid PT phone number")]}
     end
   end
+
+  defp parse_result({:ok, _number} = result), do: result
+  defp parse_result({:error, _errors}), do: :error
 end
