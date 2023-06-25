19842e36c78ecb0bcd31deabef5ef69a54b46e5d
Nelson Estevão <nelsonmestevao@proton.me>
2023-06-25 13:04:21 +0100

    Remove kaffy admin panel

--- lib/bokken_web/endpoint.ex
+++ lib/bokken_web/endpoint.ex
@@ -31,12 +31,6 @@ defmodule BokkenWeb.Endpoint do
     from: Path.expand("./priv/uploads"),
     gzip: false
 
-  plug Plug.Static,
-    at: "/kaffy",
-    from: :kaffy,
-    gzip: false,
-    only: ~w(assets)
-
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
