5f4c944899302ba71fe45f337f8fdf6b2efef50a
Afonso Santos <afonso.ds33@gmail.com>
2023-09-22 16:44:01 +0100

    upgrade ninja controller and skillcontroller

---
+++ lib/bokken_web/controllers/admin/ninja/ninja_json.ex
@@ -0,0 +1,35 @@
+defmodule BokkenWeb.Admin.NinjaJSON do
+
+
+  alias Bokken.Uploaders.Avatar
+
+  def show(%{ninjas: ninjas}) do
+    %{data: for(ninja <- ninjas, do: data(ninja))}
+  end
+
+  def data( %{ninja: ninja}) do
+    %{
+      id: ninja.id,
+      photo: Avatar.url({ninja.photo, ninja}, :thumb),
+      first_name: ninja.first_name,
+      last_name: ninja.last_name,
+      birthday: ninja.birthday,
+      belt: ninja.belt,
+      socials: ninja.socials,
+      since: ninja.inserted_at
+    }
+    |> Map.put(:guardian, guardian_attrs(ninja))
+  end
+
+  defp guardian_attrs(ninja) do
+    if Ecto.assoc_loaded?(ninja.guardian) do
+      %{
+        id: ninja.guardian.id,
+        first_name: ninja.guardian.first_name,
+        last_name: ninja.guardian.last_name
+      }
+    else
+      %{}
+    end
+  end
+end
