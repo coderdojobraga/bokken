59d96964af1051d8a9e70394a927e76dfcb39448
daniel_sp <danielsp356@gmail.com>
2023-10-09 22:36:48 +0100

    fix: ninja listings

--- test/bokken_web/controllers/skill_json_test.exs
+++ test/bokken_web/controllers/skill_json_test.exs
@@ -6,8 +6,8 @@ defmodule Bokken.SkillJSONTest do
   alias BokkenWeb.SkillJSON
 
   test "data" do
-    skill = build(:skill)
-    rendered_skill = SkillJSON.data(%{skill: skill})
+    skill = insert(:skill)
+    rendered_skill = SkillJSON.data(skill)
 
     assert rendered_skill == %{
              id: skill.id,
@@ -17,19 +17,19 @@ defmodule Bokken.SkillJSONTest do
   end
 
   test "show" do
-    skill = build(:skill)
+    skill = insert(:skill)
     rendered_skill = SkillJSON.show(%{skill: skill})
 
     assert rendered_skill == %{
-             data: SkillJSON.data(%{skill: skill})
+             data: SkillJSON.data(skill)
            }
   end
 
   test "index" do
-    skills = build_list(5, :skill)
+    skills = insert_list(5, :skill)
     rendered_skills = SkillJSON.index(%{skills: skills})
 
-    expected_data = Enum.map(skills, &SkillJSON.data(%{skill: &1}))
+    expected_data = for(skill <- skills, do: SkillJSON.data(skill))
 
     assert rendered_skills == %{
              data: expected_data
